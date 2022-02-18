66a79f99d577d02113a8cbd704e8c443f4eb0cd3
forasgarddd <73256546+forasgarddd@users.noreply.github.com>
2022-02-18 23:32:40 +0200

    initial commit

---
+++ task1.cs
@@ -0,0 +1,148 @@
+﻿namespace mp_lab1;
+
+public class Task1
+{
+    static void Main(string[] args)
+        {
+            string[] ignoreList = { "the", "for", "a", "in", "and", "at"};
+            int max = 25;
+            string word = "";
+            string[] words = Array.Empty<string>();
+            int length = 0;
+            int[] counts = Array.Empty<int>();
+            int i = 0;
+
+            using (StreamReader reader = new StreamReader(@"C:\Users\Ivan\RiderProjects\mp_lab1\mp_lab1\task1_input.txt"))
+            {
+                //Зчитуємо текст посимвольно
+                reading:
+                {
+                    if (reader.EndOfStream == true)
+                        reader.Close();
+                    char letter = (char) reader.Read();
+                    //слова з маленькими літерами
+                    if (letter is >= 'a' and <= 'z')
+                    {
+                        word = word + letter;
+                        if (reader.EndOfStream == false)
+                        {
+                            goto reading;
+                        }
+                    }
+                    //Нормалізація великих літер
+                    else if (letter is >= 'A' and <= 'Z')
+                    {
+                        word = word + ((char) (letter + 32)).ToString();
+                        if (reader.EndOfStream == false)
+                        {
+                            goto reading;
+                        }
+                    }
+                    if (word != "")
+                    {
+                        i = 0;
+                        // Перевірка на попадання у ігнор-ліст
+                        ignoreCheck:
+                        {
+                            if (word == ignoreList[i])
+                            {
+                                word = "";
+                                if (reader.EndOfStream)
+                                    reader.Close();
+                                goto reading;
+                            }
+                            i++;
+                            if (i < ignoreList.Length)
+                                goto ignoreCheck;
+                        }
+                        i = 0;
+                        // Додаємо нове слово, або збільшуємо лічильник для вже доданого
+                        newWord:
+                        {
+                            if (i == length)
+                            {
+                                goto isNew;
+                            }
+                            if (word == words[i])
+                            {
+                                counts[i]++;
+                                word = "";
+                                if (reader.EndOfStream)
+                                    reader.Close();
+                                goto reading;
+                            }
+                            i++;
+                            goto newWord;
+                        }
+                        //При додаванні нового слова, збільшуємо розмір масиву
+                        isNew:
+                        if (length >= words.Length * 0.9)
+                        {
+                            string[] newWords = new string[(length + 2) * 2];
+                            int[] newCounts = new int[(length + 2) * 2];
+                            i = 0;
+                            startCopying:
+                            {
+                                if (i == length)
+                                {
+                                    words = newWords;
+                                    counts = newCounts;
+                                    goto finishCopying;
+                                }
+                                newWords[i] = words[i];
+                                newCounts[i] = counts[i];
+                                i++;
+                                goto startCopying;
+                            }
+                        }
+                        finishCopying:
+                        words[length] = word;
+                        counts[length] = 1;
+                        word = "";
+                        length++;
+                    }
+                    if (reader.EndOfStream == false)
+                        goto reading;
+                }
+            }
+            
+            //сортування бульбашкою
+            int x = length - 1;
+            loop1:
+            {
+                int y = 0;
+                if (x >= 1)
+                {
+                    loop2:
+                    {
+                        if (y < i)
+                        {
+                            if (counts[y] < counts[y+1])
+                            {
+                                int tempCounts = counts[y];
+                                string tempWords = words[y];
+                                counts[y] = counts[y + 1];
+                                words[y] = words[y + 1];
+                                counts[y + 1] = tempCounts;
+                                words[y + 1] = tempWords;
+                            }
+                            y = y + 1;
+                            goto loop2;
+                        }
+                    }
+                    x = x - 1;
+                    goto loop1;
+                }
+            }
+            
+            i = 0;
+            writing:
+            {
+                Console.WriteLine(words[i] + " - " + counts[i]);
+                i++;
+                if (i < max && i < length)
+                    goto writing;
+            }
+            
+        }
+    }
---
+++ task2.cs
@@ -0,0 +1,269 @@
+﻿namespace mp_lab1;
+
+public class Task2
+{
+    static void Main(string[] args)
+    {
+        int ignoring = 100;
+        int curPage = 0; 
+        int[] wordsCount = new int[1]; 
+        char letter; 
+        int length = 0, index; 
+        int lines = 45;
+        string[] words = new string[1];
+        int i = 0, j = 0; 
+        int[][] contentOfPage = new int[1][];
+        string word = ""; 
+        int linesOnPage = 0; 
+        string curLine = ""; 
+        
+        using (StreamReader streamReader =
+               new StreamReader(@"C:\Users\Ivan\RiderProjects\mp_lab1\mp_lab1\task2_input.txt"))
+        {
+            //Зчитуємо текст посимвольно
+            reading:
+            {
+                if (!streamReader.EndOfStream)
+                {
+                    curLine = streamReader.ReadLine();
+                    linesOnPage = linesOnPage + 1;
+                }
+                else
+                {
+                    streamReader.Close();
+                }
+
+                if (linesOnPage == lines)
+                {
+                    linesOnPage = 0;
+                    curPage = curPage + 1;
+                }
+
+                i = 0;
+                iter:
+                {
+                    if (i == curLine.Length)
+                    {
+                        goto nextChar;
+                    }
+
+                    letter = curLine[i];
+
+                    if ('a' <= letter && letter <= 'z')
+                    {
+                        word = word + letter;
+                        if (i + 1 < curLine.Length)
+                            goto nextChar;
+                    }
+                    else if ('A' <= letter && letter <= 'Z')
+                    {
+                        word = word + (char) (letter + 32);
+                        if (i + 1 < curLine.Length)
+                            goto nextChar;
+                    }
+                    if (word != "")
+                    {
+                        index = 0;
+                        checking:
+                        {
+                            if (index == length)
+                            {
+                                goto newWord;
+                            }
+
+                            if (word ==
+                                words[index])
+                            {
+                                word = "";
+                                wordsCount[index] = wordsCount[index] + 1;
+                                if (contentOfPage[index].Length <
+                                    wordsCount[index])
+                                {
+                                    j = 0;
+                                    int[] newPages = new int[wordsCount[index] * 2];
+                                    nextCopy:
+                                    {
+                                        newPages[j] = contentOfPage[index][j];
+                                        j = j + 1;
+                                        if (j < wordsCount[index] - 1)
+                                            goto nextCopy;
+                                    }
+                                    contentOfPage[index] = newPages;
+                                    contentOfPage[index][wordsCount[index] - 1] = curPage;
+                                }
+
+                                contentOfPage[index][wordsCount[index] - 1] = curPage;
+                                goto nextChar;
+                            }
+
+                            index++;
+                            goto checking;
+                        }
+
+                        newWord:
+                        if (length ==
+                            words.Length)
+                        {
+                            string[] nw = new string[words.Length * 2];
+                            int[] nwca = new int[words.Length * 2];
+                            int[][] npa = new int[words.Length * 2][];
+                            index = 0;
+                            copyTheNextElement:
+                            {
+                                if (index == length)
+                                {
+                                    words = nw;
+                                    wordsCount = nwca;
+                                    contentOfPage = npa;
+                                    goto addTheLastWord;
+                                }
+
+                                nw[index] = words[index];
+                                nwca[index] = wordsCount[index];
+                                npa[index] = contentOfPage[index];
+                                index++;
+                                goto copyTheNextElement;
+                            }
+                        }
+
+                        addTheLastWord:
+                        {
+                            words[length] = word;
+                            wordsCount[length] = 1;
+                            contentOfPage[length] = new int[1];
+                            contentOfPage[length][0] = curPage;
+                            length++;
+                            word = "";
+                        }
+                    }
+
+                    nextChar:
+                    {
+                        i = i + 1;
+                        if (i >= curLine.Length)
+                        {
+                            goto readNextLine;
+                        }
+
+                        goto iter;
+                    }
+                }
+
+                readNextLine:
+                if (!streamReader.EndOfStream)
+                {
+                    goto reading;
+                }
+            }
+        }
+
+        //сортування бульбашкою
+        i = length - 1;
+        loop1:
+        {
+            j = 0;
+            if (i >= 1)
+            {
+                loop2:
+                {
+                    if (j < i)
+                    {
+                        int letterIndex = 0;
+                        checkTheNextChar:
+                        {
+                            if (words[j][letterIndex] == words[j + 1][letterIndex]
+                                && letterIndex + 1 != words[j].Length &&
+                                letterIndex + 1 != words[j + 1].Length)
+                            {
+                                letterIndex += 1;
+                                goto checkTheNextChar;
+                            }
+                        }
+
+                        if (letterIndex + 1 == words[j].Length && letterIndex + 1 !=
+                                                                         words[j + 1].Length
+                                                                         && words[j][letterIndex] ==
+                                                                         words[j + 1][letterIndex])
+                        {
+                            goto compareNextWord;
+                        }
+                        else if (letterIndex + 1 == words[j + 1].Length &&
+                                 letterIndex + 1 != words[j].Length
+                                 && words[j][letterIndex] == words[j + 1][letterIndex])
+                        {
+                            goto changeWords;
+                        }
+
+                        if (words[j][letterIndex] > words[j + 1][letterIndex])
+                        {
+                            goto changeWords;
+                        }
+                        else
+                        {
+                            goto compareNextWord;
+                        }
+
+                        changeWords:
+                        {
+                            int tempCount = wordsCount[j];
+                            string tempWord = words[j];
+                            var tempPages = contentOfPage[j];
+                            wordsCount[j] = wordsCount[j + 1];
+                            words[j] = words[j + 1];
+                            contentOfPage[j] = contentOfPage[j + 1];
+                            wordsCount[j + 1] = tempCount;
+                            words[j + 1] = tempWord;
+                            contentOfPage[j + 1] = tempPages;
+                        }
+                        compareNextWord:
+                        j = j + 1;
+                        goto loop2;
+                    }
+                }
+                i = i - 1;
+                goto loop1;
+            }
+        }
+        
+        index = 0;
+        writing:
+        {
+            if (wordsCount[index] < ignoring)
+            {
+                curPage = 0;
+                outp:
+                {
+                    if (curPage == 0)
+                    {
+                        Console.Write(
+                            $"{words[index]} - {contentOfPage[index][curPage]}");
+                    }
+
+                    curPage = curPage + 1;
+                    if (wordsCount[index] == curPage)
+                    {
+                        Console.WriteLine();
+                        goto endoutp;
+                    }
+
+                    if (contentOfPage[index][curPage] !=
+                        contentOfPage[index][curPage - 1])
+                    {
+                        Console.Write($", {contentOfPage[index][curPage]}");
+                    }
+
+                    goto outp;
+                }
+            }
+            endoutp:
+            {
+                index = index + 1;
+                if (index < length)
+                {
+                    goto writing;
+                }
+            }
+        }
+        
+    }
+}
